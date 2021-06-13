260ed6df40f225c6e4e5dd77c547a5da5296a20e
dhaval nagar <dhavalnagar@192.168.1.4>
2021-06-13 14:36:50 +0530

    saving session info into db

--- functions/api/create-session.js
+++ functions/api/create-session.js
@@ -1,9 +1,37 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const moment = require('moment');
+const DocumentClient = new DynamoDB.DocumentClient()
+
+const { USER_SESSION_TABLE } = process.env;
 
 module.exports.handler = async (event) => {
-    console.log(event);
+    const { id, name, email, sessionTime, duration, timezone } = JSON.parse(event.body)
+    console.log(id, name, email, sessionTime, duration, timezone);
 
-    return {
+    const result = {
         statusCode : 200,
-        body: JSON.stringify({})
+        body: ""
     };
+
+    try{    
+        const item = {
+            id: id,
+            sessionTime: sessionTime,
+            contactName: name,
+            contactEmail: email,                
+            duration: duration,
+            userTimezone: timezone || 'UTC'
+        };
+        
+        await DocumentClient.put({
+            TableName: USER_SESSION_TABLE,
+            Item: item,
+            ConditionExpression: "attribute_not_exists(id) and attribute_not_exists(sessionTime)"
+        }).promise();
+    }catch(e){
+        result.statusCode = 500
+        result.body = JSON.stringify({message: 'Invalid session time'})        
+    }
+
+    return result;
 };
